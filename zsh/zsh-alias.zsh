db8af5168bbc7d97f8efe2fee5ce5860ddcddb7b
mayermichael1 <michael.mayer2105@gmail.com>
2026-04-09 17:01:22 +0200

    create aliases for tracking uni times

--- zsh/zsh-alias.zsh
+++ zsh/zsh-alias.zsh
@@ -7,3 +7,6 @@ alias dotgit='/usr/bin/git --git-dir=$HOME/.dotgit/ --work-tree=$HOME'
 alias bake='time bear -- make'
 alias make='echo "Use bake to create compile_commands.json"; make'
 alias mvim='env NVIM_APPNAME=mvim nvim'
+
+alias tuni1='tagtime -f ~/Documents/University/2025WS/tagtime.data'
+alias tuni2='tagtime -f ~/Documents/University/2026WS/tagtime.data'
