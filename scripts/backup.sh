7b46da8d79ad63e3da9279f96b6c2579edaf3e6c
mayermichael1 <michael.mayer2105@gmail.com>
2026-09-22 21:44:15 +0200

    exlude commands relative

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -9,17 +9,17 @@ disk=/run/media/$username/BackupDisk01
 systempath=$disk/$hostname
 userpath=$systempath/$username
 
-exclude="--exclude .cache"
-exclude="$exclude --exclude .dartServer"
-exclude="$exclude --exclude .gradle"
-exclude="$exclude --exclude .git-credentials"
-exclude="$exclude --exclude .local"
-exclude="$exclude --exclude .mozilla"
-exclude="$exclude --exclude .pki"
-exclude="$exclude --exclude .steam"
-exclude="$exclude --exclude .steampid"
-exclude="$exclude --exclude .steampath"
-exclude="$exclude --exclude .z"
+exclude="--exclude ./.cache"
+exclude="$exclude --exclude ./.dartServer"
+exclude="$exclude --exclude ./.gradle"
+exclude="$exclude --exclude ./.git-credentials"
+exclude="$exclude --exclude ./.local"
+exclude="$exclude --exclude ./.mozilla"
+exclude="$exclude --exclude ./.pki"
+exclude="$exclude --exclude ./.steam"
+exclude="$exclude --exclude ./.steampid"
+exclude="$exclude --exclude ./.steampath"
+exclude="$exclude --exclude ./.z"
 
 beginnTime=$(date +%s)
 
